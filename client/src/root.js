f5d054cb1d11f9c2c43b08cfb2db0ff525ad7191
yangfan0095 <1056947355@qq.com>
2018-01-04 18:53:10 +0800

    删除冗余

--- client/src/root.js
+++
@@ -1,8 +0,0 @@
-import {Component} from 'react';
-import BasicExample from './App';
-
-export default class Root extends Component {
-  render() {
-    return BasicExample;
-  }
-}
--- server/test.js
+++
@@ -1,5 +0,0 @@
-import data from './demo.js';
-import App from './react.js';
-let a = data;
-console.log(App);
-console.log(data);
