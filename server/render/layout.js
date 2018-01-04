c6d032573dde5aa012735ff39481ba87fa1f3514
yangfan0095 <1056947355@qq.com>
2018-01-04 16:01:34 +0800

    暂时 修改客户端打包配置 ， 使之与服务端render保持一致性。实现前后端路由同步

--- server/render/layout.js
+++ server/render/layout.js
@@ -2,25 +2,28 @@
 /**
  * content 上下文 data后期需要挂在的数据
  */
+//<link href="/static/css/main.css" rel="stylesheet">
 exports.layout = function (content, data) {
     return `
   <!DOCTYPE html>
-  <html>
-  <head>
-    <meta charSet='utf-8'/>
-    <meta httpEquiv='X-UA-Compatible' content='IE=edge'/>
-    <meta name='renderer' content='webkit'/>
-    <meta name='keywords' content='demo'/>
-    <meta name='description' content='demo'/>
-    <meta name='viewport' content='width=device-width, initial-scale=1'/>
-    <link rel="stylesheet" href="/dist/css/style.css">
-  </head>
-  <body>
-    <div id="root"><div>${content}</div></div>
-  <script>
-  </script>
- <script type="text/javascript" src="/static/js/bundle.js"></script>
-  </body>
-  </html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+    <meta name="theme-color" content="#000000">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="shortcut icon" href="/favicon.ico">
+    <title>React App</title>
+    
+</head>
+
+<body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"> ${contetn}</div>
+    <script type="text/javascript" src="/static/js/main.js"></script>
+</body>
+
+</html>
 `;
 };
